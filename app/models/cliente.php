64d7c480071e5e9d91b8d927e9ae542305438b5e
Gustavo Costa Brito <gu.costabrito@gmail.com>
2025-09-08 01:16:42 +0000

    Finalizando as APIs

--- app/models/cliente.php
+++ app/models/cliente.php
@@ -1,6 +1,7 @@
 <?php
 
-class Cliente extends Database{
+class Cliente extends Database
+{
 
     // GET
     public function getEmail(string $email_hash): array|bool
@@ -28,7 +29,7 @@ class Cliente extends Database{
 
     public function getWhatsappAtu(string $whatsapp_hash, int $id): array|bool
     {
-        $sql = "SELECT * FROM tbl_cliente WHERE whatsapp_hash = :whatsapp AND id_cliente = :id AND status_cliente = 'Ativo'";
+        $sql = "SELECT * FROM tbl_cliente WHERE whatsapp_hash = :whatsapp AND id_cliente != :id AND status_cliente = 'Ativo'";
 
         $stmt = $this->db->prepare($sql);
         $stmt->execute([
@@ -49,7 +50,8 @@ class Cliente extends Database{
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function getAgendamentoComentario($id){
+    public function getAgendamentoComentario($id)
+    {
         $sql = "SELECT * FROM tbl_cliente
         INNER JOIN tbl_agendamento ON tbl_cliente.id_cliente = tbl_agendamento.id_cliente
         INNER JOIN tbl_comentario ON tbl_cliente.id_cliente = tbl_comentario.id_cliente
@@ -75,7 +77,6 @@ class Cliente extends Database{
 
 
     // ADD
-
     public function addCadastro(array $campos): int
     {
         extract($campos);
@@ -104,33 +105,80 @@ class Cliente extends Database{
     {
         extract($campos);
 
-        $sql = "UPDATE tbl_cliente SET 
-        nome_cliente = :nome,
-        email_cliente = :email,
-        email_hash = :email_hash,
-        whatsapp_cliente = :whatsapp,
-        whatsapp_hash = :whatsapp_hash,
-        senha_cliente = :senha
-        WHERE id_cliente = :id";
+        if(is_bool($senha)){
+            $sql = "UPDATE tbl_cliente SET
+            nome_cliente = :nome,
+            email_cliente = :email,
+            email_hash = :email_hash,
+            whatsapp_cliente = :whatsapp,
+            whatsapp_hash = :whatsapp_hash
+            WHERE id_cliente = :cliente";
+        } else {
+            $sql = "UPDATE tbl_cliente SET
+            nome_cliente = :nome,
+            email_cliente = :email,
+            email_hash = :email_hash,
+            whatsapp_cliente = :whatsapp,
+            whatsapp_hash = :whatsapp_hash,
+            senha_cliente = :senha
+            WHERE id_cliente = :cliente";
+        }
 
         $stmt = $this->db->prepare($sql);
-        return $stmt->execute([
-            ':id' => $id,
-            ':nome' => $nome,
-            ':email' => $email,
-            ':email_hash' => $email_hash,
-            ':whatsapp' => $whatsapp,
-            ':whatsapp_hash' => $whatsapp_hash,
-            ':senha_cliente' => $senha ?? NULL
-        ]);
+
+        $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':email_hash', $email_hash, PDO::PARAM_STR);
+        $stmt->bindValue(':whatsapp', $whatsapp, PDO::PARAM_STR);
+        $stmt->bindValue(':whatsapp_hash', $whatsapp_hash, PDO::PARAM_STR);
+        
+        if(is_string($senha)){
+            $stmt->bindValue(':senha', $senha, PDO::PARAM_STR);
+        }
+
+        $stmt->bindValue(':cliente', $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
     }
 
-    public function updateEstrela($cliente){
-        $sql = "UPDATE tbl_cliente SET estrela_cliente = 1 WHERE id_cliente = :cliente";
 
-        $stmt = $this->db->prepare($sql);
-        return $stmt->execute([
-            ':cliente' => (int)$cliente
+
+
+    // DELETE
+    public function deleteCadastro(int $id): bool
+    {
+        $sql1 = "DELETE FROM tbl_cliente WHERE id_cliente = :cliente";
+        $sql2 = "DELETE FROM tbl_agendamento WHERE id_cliente = :cliente";
+        $sql3 = "DELETE FROM tbl_comentario WHERE id_cliente = :cliente";
+
+        $this->db->beginTransaction();
+
+        $stmt1 = $this->db->prepare($sql1);
+        $stmt2 = $this->db->prepare($sql2);
+        $stmt3 = $this->db->prepare($sql3);
+
+
+        $resposta[] = $stmt1->execute([
+            ':cliente' => $id
         ]);
+
+        $resposta[] = $stmt2->execute([
+            ':cliente' => $id
+        ]);
+
+        $resposta[] = $stmt3->execute([
+            ':cliente' => $id
+        ]);
+
+
+        foreach ($resposta as $valor) {
+            if (!$valor) {
+                $this->db->rollback();
+                return false;
+            }
+        }
+
+        $this->db->commit();
+        return true;
     }
 }
